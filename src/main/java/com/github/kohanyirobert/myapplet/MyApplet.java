48b74380a1e391f00356ce1a92e41f14df68c4ec
Robert Kohanyi <robert.kohanyi@metapack.com>
2016-04-19 12:10:43 +0100

    Initial commit

---
+++ src/main/java/com/github/kohanyirobert/myapplet/MyApplet.java
@@ -0,0 +1,10 @@
+package com.github.kohanyirobert.myapplet;
+
+import java.applet.Applet;
+
+public class MyApplet extends Applet {
+
+  public void init() {
+    System.out.println("Hello World!");
+  }
+}
